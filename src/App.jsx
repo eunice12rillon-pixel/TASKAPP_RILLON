900a56ba579cfe2e48b319be60c9faaf551bb680
eunice12rillon-pixel <eunice12rillon@gmail.com>
2026-02-03 20:16:14 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import backgroundImage from "./assets/paperbackground.png";
 import "./App.css";
 
 function App() {
   return (
     <div
       className="min-h-screen bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: "url('/paperbackground.png')" }}
+      style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <Header />
       <main className="max-w-4xl mx-auto py-8 px-4">
