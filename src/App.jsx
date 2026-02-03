3eccda0a9b7c871d64a0f33bc18af875075803d4
eunice12rillon-pixel <eunice12rillon@gmail.com>
2026-02-03 19:45:26 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="min-h-screen bg-cover bg-center bg-no-repeat"
-    style={{backgroundImage: "url('public/paperbackground.png')" }}
+    <div
+      className="min-h-screen bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: "url('/paperbackground.png')" }}
     >
-      <Header /> 
+      <Header />
       <main className="max-w-4xl mx-auto py-8 px-4">
         <Outlet />
       </main>
